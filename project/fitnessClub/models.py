0bf03ba42c451473ea03fd3654b609d0493f6f5c
Nirtkor <itnirtkor@gmail.com>
2023-11-21 02:55:21 +0300

    flake fix test_models

--- project/fitnessClub/models.py
+++ project/fitnessClub/models.py
@@ -1,16 +1,19 @@
 from django.db import models
 from django.contrib.auth import get_user_model
 
+
 class Client(models.Model):
     client_id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100)
     address = models.CharField(max_length=100)
     phone = models.CharField(max_length=20)
 
+
 class Trainer(models.Model):
     trainer_id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100)
     speciality = models.CharField(max_length=50)
+
 
 class Class(models.Model):
     class_id = models.AutoField(primary_key=True)
@@ -20,6 +23,7 @@ class Class(models.Model):
     start_time = models.TimeField()
     end_time = models.TimeField()
 
+
 class Membership(models.Model):
     membership_id = models.AutoField(primary_key=True)
     client = models.ForeignKey(Client, on_delete=models.CASCADE)
@@ -27,11 +31,13 @@ class Membership(models.Model):
     end_date = models.DateField()
     cost = models.DecimalField(max_digits=10, decimal_places=2)
 
+
 class Subscription(models.Model):
     subscription_id = models.AutoField(primary_key=True)
     client = models.ForeignKey(Client, on_delete=models.CASCADE)
     class_field = models.ForeignKey(Class, on_delete=models.CASCADE, db_column='class_id')
     subscription_date = models.DateField()
+
 
 class IndividualTraining(models.Model):
     user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
--- project/tests/test_models.py
+++ project/tests/test_models.py
@@ -6,7 +6,9 @@ from django.contrib.auth import get_user_model
 
 class ModelsTest(TestCase):
     def setUp(self):
-        self.user, _ = get_user_model().objects.get_or_create(username='testuser', defaults={'password': 'testpassword'})
+        self.user, _ = get_user_model().objects.get_or_create(username='testuser', 
+                                                              defaults={'password': 'testpassword'},
+                                                              )
 
     def test_client_model(self):
         client = Client.objects.create(name='Test Client', address='Test Address', phone='1234567890')
@@ -21,24 +23,41 @@ class ModelsTest(TestCase):
 
     def test_class_model(self):
         trainer = Trainer.objects.create(name='Test Trainer', speciality='Test Speciality')
-        class_instance = Class.objects.create(class_name='Test Class', trainer=trainer, class_date='2023-01-01', start_time='12:00:00', end_time='13:00:00')
+        class_instance = Class.objects.create(class_name='Test Class', 
+                                              trainer=trainer, 
+                                              class_date='2023-01-01', 
+                                              start_time='12:00:00', 
+                                              end_time='13:00:00',
+                                              )
         self.assertEqual(class_instance.class_name, 'Test Class')
         self.assertEqual(class_instance.trainer, trainer)
 
     def test_membership_model(self):
         client = Client.objects.create(name='Test Client', address='Test Address', phone='1234567890')
-        membership = Membership.objects.create(client=client, start_date='2023-01-01', end_date='2023-12-31', cost=100.00)
+        membership = Membership.objects.create(client=client, 
+                                               start_date='2023-01-01', 
+                                               end_date='2023-12-31', 
+                                               cost=100,
+                                               )
         self.assertEqual(membership.client, client)
         self.assertEqual(membership.cost, 100.00)
 
     def test_subscription_model(self):
         client = Client.objects.create(name='Test Client', address='Test Address', phone='1234567890')
         trainer = Trainer.objects.create(name='Test Trainer', speciality='Test Speciality')
-        class_instance = Class.objects.create(class_name='Test Class', trainer=trainer, class_date='2023-01-01', start_time='12:00:00', end_time='13:00:00')
-        subscription = Subscription.objects.create(client=client, class_field=class_instance, subscription_date='2023-01-01')
+        class_instance = Class.objects.create(class_name='Test Class', 
+                                              trainer=trainer, 
+                                              class_date='2023-01-01', 
+                                              start_time='12:00:00', 
+                                              end_time='13:00:00',
+                                              )
+        subscription = Subscription.objects.create(client=client, 
+                                                   class_field=class_instance, 
+                                                   subscription_date='2023-01-01',
+                                                   )
         self.assertEqual(subscription.client, client)
         self.assertEqual(subscription.class_field, class_instance)
-    
+
     def test_individual_training_model(self):
         training = IndividualTraining.objects.create(user=self.user, date='2023-01-01', time='12:00:00')
         self.assertEqual(training.user, self.user)
