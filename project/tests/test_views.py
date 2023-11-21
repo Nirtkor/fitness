2f7eb52fe3806285ef5630059b1a893c09155208
Nirtkor <itnirtkor@gmail.com>
2023-11-21 03:35:05 +0300

    flake fix admin and test_views

--- project/tests/test_views.py
+++ project/tests/test_views.py
@@ -1,9 +1,9 @@
-from django.contrib.auth import get_user_model
 from django.test import TestCase
 from rest_framework import status
-from rest_framework.test import APIClient
 from fitnessClub.models import Client
 from django.urls import reverse
+
+EXPECTED_STATUS_CODE = 200
 
 
 class ClientViewTest(TestCase):
@@ -12,14 +12,14 @@ class ClientViewTest(TestCase):
 
     def test_create_client(self):
         url = reverse('clients-list')
-        data = {
+        test_data = {
             'name': 'Test Client',
             'address': 'Test Address',
-            'phone': '1234567890'
+            'phone': '1234567890',
         }
-        response = self.client.post(url, data)
+        response = self.client.post(url, test_data)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-    
+
     def test_delete_client(self):
         url = reverse('clients-detail', args=[self.client_instance.client_id])
 
@@ -31,11 +31,11 @@ class ClientViewTest(TestCase):
 
     def test_main_page_view(self):
         response = self.client.get(reverse('main-page'))
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, EXPECTED_STATUS_CODE)
 
     def test_calendar_page_view_authenticated(self):
         response = self.client.get(reverse('calendar-page'))
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, EXPECTED_STATUS_CODE)
 
     def tearDown(self):
         self.client.logout()
